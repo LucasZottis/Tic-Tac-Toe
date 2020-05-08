69d5cc6fd20aa0ae67f8829aacd1af79800b7022
Lucas Zottis <zottis.lucas@hotmail.com>
2020-05-08 12:58:54 -0300

    Criação do projeto Tic Tac Toe

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+    Nome do programa: Tic Tac Toe
+    Objetivo do programa: Fazer um jogo da velha para console
+    Nome do programador: Lucas Zottis
+    Data de criação: 08/05/2020
+*/
+
+// * Cabeçalhos *
+#include <stdio.h>
+#include <stdlib.h>
+
+// * Estruturas *
+
+
+// * Variaveis Globais *
+
+
+// * Declaração de funções *
+
+
+int main()
+{
+    printf("Hello Tic Tac Toe!\n");
+    return 0;
+}
