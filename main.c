15bab998b51b00a3d64521b9969b05ba689e3d44
Lucas Zottis <zottis.lucas@hotmail.com>
2020-05-08 13:05:24 -0300

    Definição das estruturas e variaveis globais

--- main.c
+++ main.c
@@ -10,10 +10,16 @@
 #include <stdlib.h>
 
 // * Estruturas *
-
+typedef struct { // Dados sobre os players (a maquina conta como player e será inicializado no começo do jogo);
+	char nome[10]; // Nome do player, a maquina será COM;
+	int partidas, vitorias, empates, derrotas; // Dados sobre partidas, vitórias, empates e derrotas;
+}Jogador;
 
 // * Variaveis Globais *
-
+char grade[]
+Jogador p1; // Jogador 1
+Jogador p2; // Jogador 2
+Jogador com; // Jogador 3
 
 // * Declaração de funções *
 
